4cde27e7873d70348aef844b50a433eb9d927407
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2020-12-04 00:16:28 +0200

    authentication, register and log in

--- app_authentication/forms.py
+++ app_authentication/forms.py
@@ -1,8 +1,30 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from app_authentication.models import UserProfile
 
-class RegisterForm(forms.ModelForm):
+
+class RegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'password')
+        fields = ('username', 'email')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email', False)
+        if not email:
+            raise forms.ValidationError('Email is required')
+        return email
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('address', 'city')
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(
+        widget=forms.PasswordInput()
+    )
--- app_authentication/urls.py
+++ app_authentication/urls.py
@@ -5,7 +5,6 @@ from app_authentication import views
 
 urlpatterns = [
     path('login/', views.login_user, name='login-user'),
-    path('login-page/', views.login_page, name='login-page'),
     path('logout/', views.logout_user, name='logout-user'),
     path('register/', views.register, name='register-user'),
 ]
